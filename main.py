31ccc8af6db05f7789c7e54cdb4af8b7718cd988
NestorAngeles <nestorivan.27@gmail.com>
2020-11-25 22:40:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,50 @@
 
 from order import Order
 from pizza import Pizza
+from canvas import msjBuild, sizes, titulo, toppings
 
-# Agregar logica principal
+AFFIRM = ['s'  ,'S']
+NEGA = ['n' , 'N']
+
+menuLoop = True
+orderLoop = True
+pNro = 1
+
+
+if __name__ == "__main__":
+
+    currentOrder = Order()
+
+    while(orderLoop):
+        titulo(pNro)
+        size = sizes()
+        tps = toppings()
+
+        pizza = Pizza(size , tps , pNro)
+        msjBuild(size , tps)
+        pizza.subtotal()
+        currentOrder.addsubTotal(pizza.total)
+        resp = input('¿Desea Continuar?(s/n) : ')
+
+        if((resp not in AFFIRM) and (resp not in NEGA)):
+            print('Eleccion Invalida...')
+
+            while((resp not in AFFIRM) and (resp not in NEGA)):
+                resp = input('¿Desea Continuar?(s/n) : ')
+
+                if((resp not in AFFIRM) and (resp not in NEGA)):
+                    print('Eleccion Invalida...')
+                else:
+                    break
+        
+        if(resp in AFFIRM):
+            pNro += 1
+            currentOrder.pizzas.append(pizza)
+        else:
+            print('\nEl pedido tiene un total de ' , pNro , ' Pizza(s) por un monto de: ' , str(currentOrder.orderTotal) + '\n')
+            print('Gracias por su Compra vuelva pronto !!! \n')
+            orderLoop = False
+            break
+
+
+                
